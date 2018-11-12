05c2507fa1461ae1f695321205592410ff4c20b3
Keita TAMANAHA <e185755@ie.u-ryukyu.ac.jp>
2018-11-12 20:23:21 +0900

    ユニットテスト１

---
+++ src/test/jp/ac/uryukyu/ie/e185755/EnemyTest.java
@@ -0,0 +1,16 @@
+package jp.ac.uryukyu.ie.e185755;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EnemyTest {
+
+    @Test
+    void attack() {
+    }
+
+    @Test
+    void attack1() {
+    }
+}
